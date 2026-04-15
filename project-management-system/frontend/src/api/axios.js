9708c5efb5aea5cdcba2f04b1bb909d6d1190515
sai charan <utukurisaicharan123@gmail.com>
2026-04-16 00:21:58 +0530

    added backend url for api requests in front end

--- project-management-system/frontend/src/api/axios.js
+++ project-management-system/frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://localhost:5000/api" || " https://project-management-system-fpxs.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
